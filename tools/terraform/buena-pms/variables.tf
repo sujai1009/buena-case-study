e75fac6bd997c5b79dfb320c900ac92dff130468
sujai1009@gmail.com <sujai1009@gmail.com>
2026-01-09 21:27:52 +0100

    adding missed files

--- tools/terraform/buena-pms/variables.tf
+++ tools/terraform/buena-pms/variables.tf
@@ -1,17 +1,14 @@
 variable "pms_database_admin" {
-  type        = string                     # The type of the variable, in this case a string
-  default     = "admin"                 # Default value for the variable
-  description = "The type of EC2 instance" # Description of what this variable represents
+  type        = string
+  default     = "admin"
 }
 
 variable "pms_database_admin_password" {
-  type        = string                     # The type of the variable, in this case a string
-  default     = "password"                 # Default value for the variable
-  description = "The type of EC2 instance" # Description of what this variable represents
+  type        = string
+  default     = "password"
 }
 
 variable "pms_database_name" {
-  type        = string                     # The type of the variable, in this case a string
-  default     = "buena_pms"                 # Default value for the variable
-  description = "The type of EC2 instance" # Description of what this variable represents
+  type        = string
+  default     = "buena_pms"
 }
